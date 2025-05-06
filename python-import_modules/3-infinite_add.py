55f23ed7864135974e1402742dd62fd0d5c02ffc
N4yth <nathan.dupuis31@gmail.com>
2025-05-06 10:36:58 +0200

    add: create the python function that additionante all

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    result = 0
+    for i in range(1, len(argv)):
+        result += int(argv[i])
+    print("{}".format(result))
