e05b4c4921bba34d3312747166db36ac88462630
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 15:21:07 +0200

    fix: handle the case where the given Roman number is not a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     result = 0
     tmp_old_i = ""
-    if (roman_string is None):
+    if (roman_string is None or type(roman_string) != str):
         return (0)
     data = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in roman_string:
