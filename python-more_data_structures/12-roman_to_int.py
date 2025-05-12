214845723d4070be0f0bd5236d9c66a5f3409da2
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 15:47:07 +0200

    styles : correct style on the comment and on the if statement

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     result = 0
-    tmp_old_i = "M" #put the highest letter possible to prevent to enter in the if on the first itereation
-    if (roman_string is None or type(roman_string) != str):
+    tmp_old_i = "M"
+    # put the highest letter possible to prevent to enter in
+    # the if on the first itereation
+    if (roman_string is None or not isinstance(roman_string, str)):
         return (0)
     data = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in roman_string:
