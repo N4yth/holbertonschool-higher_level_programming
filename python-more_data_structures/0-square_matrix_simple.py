d565926bb2238591b2487bca05d98073a13bf826
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 11:36:04 +0200

    fix : change the methode of square and correct style issue

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,10 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    square = lambda a : a * a
     for row in range(0, len(matrix)):
         new_matrix.append(list(map(square, matrix[row])))
     return (new_matrix)
+
+
+def square(n):
+    return (n * n)
