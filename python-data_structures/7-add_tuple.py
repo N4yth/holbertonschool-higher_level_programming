960d8b714cf8edd0676dd9460b9ee8b6221a809b
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 09:51:54 +0200

    style : add a white space around the operator l14

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -11,5 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         for i in range(len(tuple_b), 3):
             tmp_list.append(0)
         tuple_b = tuple(tmp_list)
-    res_tuple=(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    res_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return (res_tuple)
