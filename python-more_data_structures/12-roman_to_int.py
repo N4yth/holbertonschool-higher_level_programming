f243543c0d8735152def97bab1725d585edb7de5
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 15:15:59 +0200

    add : create the python function that translate roman number into int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    tmp_old_i = ""
+    if (roman_string is None):
+        return (0)
+    data = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i in roman_string:
+        if (
+            (i == "X" and tmp_old_i == "I") or
+            (i == "V" and tmp_old_i == "I")
+        ):
+            result += data[i] - 2
+        else:
+            result += data[i]
+        tmp_old_i = i
+    return (result)
