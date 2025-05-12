639c43d944cda98f554ba197828d355a492684ff
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 16:48:54 +0200

    fix : remove useless variable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    resu = ""
+    printed = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            resu += str(my_list[i])
+            printed += 1
         except IndexError:
-            i -= 1
             break
     print("")
-    return (i + 1)
+    return (printed)
