5a3c9f8f1fd207daf50299a42500fd025b7c66bd
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 15:27:55 +0200

    fix : handle the case where no score is found but the dict is not empty

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,9 +3,11 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return (None)
-    score = 0
+    score = -1
     for i in a_dictionary:
         if (a_dictionary[i] > score):
             score = a_dictionary[i]
             biggest = i
+    if (score == -1):
+        return (None)
     return (biggest)
