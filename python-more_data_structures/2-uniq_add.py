3ee400088a795f01db0f90a28c4a7eba5751edb0
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 13:17:25 +0200

    fix : remove useless print

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,6 @@
 def uniq_add(my_list=[]):
     result = 0
     for idx in range(0, len(my_list)):
-        print(my_list[idx])
         if (my_list[idx] not in my_list[:idx]):
             result += my_list[idx]
     return (result)
