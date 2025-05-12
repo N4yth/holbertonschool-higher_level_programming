785833082a023fa82a182265e80824939d87808e
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 15:41:19 +0200

    fix : change the test when the Letter is greater then the older letter

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     result = 0
-    tmp_old_i = ""
+    tmp_old_i = "M" #put the highest letter possible to prevent to enter in the if on the first itereation
     if (roman_string is None or type(roman_string) != str):
         return (0)
     data = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in roman_string:
-        if (
-            (i == "X" and tmp_old_i == "I") or
-            (i == "V" and tmp_old_i == "I")
-        ):
-            result += data[i] - 2
+        if (data[i] > data[tmp_old_i]):
+            result += data[i] - data[tmp_old_i] * 2
         else:
             result += data[i]
         tmp_old_i = i
