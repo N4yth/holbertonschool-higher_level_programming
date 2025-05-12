f4fe9524fef193473706e461852bea10904a399b
N4yth <nathan.dupuis31@gmail.com>
2025-05-12 10:45:05 +0200

    fix : handle the start value of the biggest variable

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if (my_list is None or len(my_list) == 0):
+    if (len(my_list) == 0):
         return (None)
-    biggest = 0
+    biggest = my_list[0]
     for i in my_list:
         if (i > biggest):
             biggest = i
