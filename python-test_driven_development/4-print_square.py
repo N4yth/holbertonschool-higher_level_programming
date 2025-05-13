67ab9976959848d1e638dc14825e977539aa3732
N4yth <nathan.dupuis31@gmail.com>
2025-05-13 22:40:44 +0200

    style : correct style according to pycodestyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,14 +9,13 @@ this module contain a function that create a square made of '#'
 def print_square(size):
     """
     print a square of '#'
-    
     Args:
         size (int): the size of the square
     """
     if (not isinstance(size, int)):
-        raise TypeError ("size must be an integer")
+        raise TypeError("size must be an integer")
     if (size < 0):
-        raise ValueError  ("size must be >= 0")
+        raise ValueError("size must be >= 0")
     for row in range(0, size):
         for column in range(0, size):
             print("#", end="")
