7e2d2e652d0a54131ce9cdefc1a3015910bd4204
N4yth <nathan.dupuis31@gmail.com>
2025-05-13 12:10:11 +0200

    add : create a python function that add two number

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+0-add_integer.py
+ce module contient une fonction qui permet de faire la somme de 2 entier
+raise une exectpion cas de format non conforme
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Return the sum of a and b
+    """
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an intege")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an intege")
+    if (isinstance(a, float)):
+        a = int(a)
+    if (isinstance(b, float)):
+        b = int(b)
+    return (a + b)
