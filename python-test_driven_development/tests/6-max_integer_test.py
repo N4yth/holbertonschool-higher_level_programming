65fe92a89ee4f2c8e7147bf17830ccedacc0ebdf
N4yth <nathan.dupuis31@gmail.com>
2025-05-14 19:03:01 +0200

    add some test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,14 +10,27 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
+    def test_empty_list_tuple(self):
+        self.assertEqual(max_integer(()), None)
+
     def test_multiple_int(self):
         self.assertEqual(max_integer([1, 3, 2]), 3)
+
+    def test_multiple_int_tuple(self):
+        self.assertEqual(max_integer((1, 3, 2)), 3)
 
     def test_one_int(self):
         self.assertEqual(max_integer([45]), 45)
 
+    def test_one_int_tuple(self):
+        with self.assertRaises(TypeError):
+            max_integer((45))
+
     def test_negative_elements(self):
         self.assertEqual(max_integer([-4, -2, -3]), -2)
+
+    def test_negative_elements_tuple(self):
+        self.assertEqual(max_integer((-4, -2, -3)), -2)
 
     def test_float(self):
         self.assertEqual(max_integer([1.0, 2.5, 7.1, 0.9]), 7.1)
@@ -37,6 +50,13 @@ class TestMaxInteger(unittest.TestCase):
     def test_list_string(self):
         self.assertEqual(max_integer(["6", "45", "33", "9.2"]), "9.2")
 
+    def test_tuple_string(self):
+        self.assertEqual(max_integer(("6", "45", "33", "9.2")), "9.2")
+
+    def test_list_string_int(self):
+        with self.assertRaises(TypeError):
+            max_integer(["6", 45, "33", "9.2"])
+
     def test_None_element(self):
         with self.assertRaises(TypeError):
             max_integer(["6", "45", None, "9.2"])
@@ -55,6 +75,10 @@ class TestMaxInteger(unittest.TestCase):
     def test_not_list_dict(self):
         with self.assertRaises(KeyError):
             max_integer({"toto": 3, "jsp": 4, "GG": 999, "legume": 0})
+
+    def test_not_list_reverse_dict(self):
+        with self.assertRaises(KeyError):
+            max_integer({3: "toto", 4: "jsp", 999: "GG", 0: "legume"})
 
 
 if __name__ == '__main__':
