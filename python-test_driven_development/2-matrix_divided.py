aba286bb29af01b568668db53f367e567e8c00e7
N4yth <nathan.dupuis31@gmail.com>
2025-05-14 09:13:49 +0200

    fix : handle the case where the matrix is only 1 list and reorganize the code

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,21 +12,25 @@ def matrix_divided(matrix, div):
     devided all number of a matrix by a given data
     """
     new_matrix = []
-    if (div == 0):
-        raise ZeroDivisionError("division by zero")
-    for row in range(0, len(matrix) - 1):
-        if (len(matrix[row]) != len(matrix[row + 1])):
+    len_ref = len(matrix[0])
+    check = False
+    for row in range(0, len(matrix)):
+        for column in range(0, len(matrix[row])):
+            actual = matrix[row][column]
+            if (not isinstance(actual, int) and not isinstance(actual, float)):
+                raise TypeError(
+                    "matrix must be a matrix "
+                    "(list of lists) of integers/floats")
+        if (len(matrix[row]) != len_ref):
             raise TypeError("Each row of the matrix must have the same size")
     if (not isinstance(div, int) and not isinstance(div, float)):
         raise TypeError("div must be a number")
+    if (div == 0):
+        raise ZeroDivisionError("division by zero")
     for row in matrix:
         tmp_list = []
         for column in row:
             result = 0
-            if (not isinstance(column, int) and not isinstance(column, float)):
-                raise TypeError(
-                    "matrix must be a matrix "
-                    "(list of lists) of integers/floats")
             result = round(column / div, 2)
             tmp_list.append(result)
         new_matrix.append(tmp_list)
