f75eac8678cdf47a64a70f51017a2d12ba18a186
N4yth <nathan.dupuis31@gmail.com>
2025-05-14 20:13:30 +0200

    fix : recode the whole function white usefull function to simplify all the old code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,26 +12,14 @@ def text_indentation(text):
     Args:
         size (int): the size of the square
     """
-    new_line = True
+    Line = ""
     if (not isinstance(text, str)):
         raise TypeError("text must be a string")
-    for char in range(0,len(text)):
-        if (text[char] == '.' or text[char] == '?' or text[char] == ':'):
-            print("{}\n".format(text[char]))
-            new_line = True
-        else:
-            if (new_line == True and text[char].isspace()):
-                continue
-            elif (text[char].isspace()):
-                test = char
-                while (text[test].isspace()):
-                    test += 1
-                    if (not text[test].isspace()):
-                        break
-                    else:
-                        try:
-                            text[test].isspace()
-                        except IndexError:
-                            return()
-            new_line = False
-            print(text[char], end="")
+    for char in text:
+        Line += char
+        if char in ".:?":
+            print(Line.strip())
+            print()
+            Line = ""
+    if Line.strip():
+        print(Line.strip(), end="")
