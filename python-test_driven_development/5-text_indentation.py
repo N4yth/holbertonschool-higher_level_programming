4eb28921ad3f019b2466553aaa96db880bd353fd
N4yth <nathan.dupuis31@gmail.com>
2025-05-14 19:34:33 +0200

    add test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,10 +12,15 @@ def text_indentation(text):
     Args:
         size (int): the size of the square
     """
+    new_line = True
     if (not isinstance(text, str)):
         raise TypeError("text must be a string")
     for char in text:
         if (char == '.' or char == '?' or char == ':'):
             print("{}\n".format(char))
+            new_line = True
         else:
+            if (new_line == True and char.isspace()):
+                continue
+            new_line = False
             print(char, end="")
