5f67c3cf36877ccfc5f20f3ee3d7fda7a990a5e6
N4yth <nathan.dupuis31@gmail.com>
2025-05-19 13:40:32 +0200

    fix : correct the test of the type of value of the position tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
         elif (len(position) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (not isinstance(position[0], int) or
-                not isinstance(position[0], int)):
+                not isinstance(position[1], int)):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
