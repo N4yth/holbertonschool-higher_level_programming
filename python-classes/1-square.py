1e2639b86d764baa0b885205c5aaafb3a2f7adce
N4yth <nathan.dupuis31@gmail.com>
2025-05-19 09:12:08 +0200

    add : create the class square with the private atribue size

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module that create a square class
+"""
+
+
+class Square:
+    """
+    a class that represente square
+    """
+    def __init__(self, size):
+        self._Square__size = size
