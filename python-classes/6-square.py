3ed90afbad0855060d6237aa079e23eb764bad8c
N4yth <nathan.dupuis31@gmail.com>
2025-05-19 10:34:05 +0200

    add : create the python methode getter and setter for position and modifie the way to print

---
+++ python-classes/6-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+"""
+module that create a square class
+"""
+
+
+class Square(object):
+    """
+    a class that represente square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+        if (len(position) != 2):
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self._Square__position = position
+
+    @property
+    def size(self):
+        """
+        methode that return the size
+        """
+        return self._Square__size
+
+    @property
+    def position(self):
+        """
+        methode that return the position
+        """
+        return self._Square__position
+
+    @size.setter
+    def size(self, value):
+        """
+        methode set a new size
+        """
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    @position.setter
+    def position(self, value):
+        """
+        methode set a new position
+        """
+        if (len(value) != 2):
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self._Square__position = value
+
+    def area(self):
+        """
+        methode that return the area of the square
+        """
+        return (self._Square__size * self._Square__size)
+
+    def my_print(self):
+        """
+        methode that print the square
+        """
+        if (self._Square__size == 0):
+            print("")
+        else:
+            for row in range(0, self._Square__size):
+                for column in range(0, self._Square__size):
+                    if (column == 0):
+                        for i in range(0, self._Square__position[0]):
+                            if self._Square__position[1] > 0:
+                                print("_", end="")
+                            else:
+                                print(" ", end="")
+                    print("#", end="")
+                print("")
