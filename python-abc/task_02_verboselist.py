9d2122896d839a445be55155590297481d093cda
N4yth <nathan.dupuis31@gmail.com>
2025-05-26 17:18:14 +0200

    fix : handle the case where the remove not work and print before remove

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -2,19 +2,22 @@
 
 class VerboseList(list):
     def append(self, item):
-        super(VerboseList, self).append(item)
+        super().append(item)
         print("Added [{}] to the list.".format(item))
 
     def extend(self, item):
-        super(VerboseList, self).extend(item)
+        super().extend(item)
         print("Extended the list with [{}] items.".format(len(item)))
 
     def remove(self, item):
-        super(VerboseList, self).remove(item)
         print("Removed [{}] from the list.".format(item))
+        try:
+            super().remove(item)
+        except ValueError:
+            raise ValueError
 
     def pop(self, item=None):
         if item is None:
             item = len(self) - 1
-        resu = super(VerboseList, self).pop(item)
-        print("Popped [{}] from the list.".format(resu))
+        print("Popped [{}] from the list.".format(self[item]))
+        resu = super().pop(item)
