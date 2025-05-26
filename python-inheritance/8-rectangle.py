d71e60dee8e055879db25ce7082784db28f2c58f
N4yth <nathan.dupuis31@gmail.com>
2025-05-26 12:24:24 +0200

    change the name send to the test value

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(BaseGeometry):
     this class is a child of BaseGeometry and represente Rectangle
     """
     def __init__(self, width, height):
-        self.integer_validator("Rectangle", width)
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("Rectangle", height)
+        self.integer_validator("height", height)
         self.__height = height
