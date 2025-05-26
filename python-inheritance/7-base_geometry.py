f33536740a85dce14873ea8820be6b390751c126
N4yth <nathan.dupuis31@gmail.com>
2025-05-26 10:55:45 +0200

    add : test and function that valide if number given is greater than 0 and a int

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+this module have a class of geometry
+"""
+
+
+class BaseGeometry:
+    """
+    class of geometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if (not isinstance(value, int)):
+            raise TypeError("{} must be an integer".format(name))
+        if (value < 1):
+            raise ValueError("{} must be greater than 0".format(name))
