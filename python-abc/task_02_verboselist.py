1e285036984a1cdc5924e1967b2201a10dd4a7f2
N4yth <nathan.dupuis31@gmail.com>
2025-05-27 11:14:28 +0200

    fix : use condition and do not raise an error

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -13,14 +13,10 @@ class VerboseList(list):
         if item in self:
             print("Removed [{}] from the list.".format(item))
             super().remove(item)
-        else:
-            raise ValueError("list.remove(x): x not in list")
 
     def pop(self, item=None):
         if item is None:
             item = len(self) - 1
-        try:
+        if (len(self) >= item):
             print("Popped [{}] from the list.".format(self[item]))
             resu = super().pop(item)
-        except IndexError:
-            raise IndexError("pop index out of range")
