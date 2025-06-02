2ed13335059b574642b040a6fe9a05f2ea681ab2
N4yth <nathan.dupuis31@gmail.com>
2025-06-02 13:51:34 +0200

    add : create the python module that return the content of a json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+This module contain 1 function that append string in text
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    this function to append string in a file
+    '''
+    return json.dumps(my_obj)
