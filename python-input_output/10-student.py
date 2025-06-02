3e71d0d9f09c315068afc13fa74f4f1d092f2bd1
N4yth <nathan.dupuis31@gmail.com>
2025-06-02 19:46:45 +0200

    fix : change the creation order of attribute

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,9 +10,9 @@ class Student:
     class that represent students
     '''
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
         self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
 
     def to_json(self, attrs=None):
         '''
