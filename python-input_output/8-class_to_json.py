ac84ed96c139057261a72c6a0f7be87a047bcd8d
N4yth <nathan.dupuis31@gmail.com>
2025-06-02 19:07:42 +0200

    pycodestyle correct

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,7 +7,7 @@ This module contain 1 function
 
 def class_to_json(obj):
     '''
-    function that returns the dictionary description with 
+    function that returns the dictionary description with
     simple data structure
     '''
     return obj.__dict__
