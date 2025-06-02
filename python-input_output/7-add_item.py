6b6f0b6b47cdb7701ebe6df551ae7a94a43118ac
N4yth <nathan.dupuis31@gmail.com>
2025-06-02 16:37:05 +0200

    add : create the python module that  adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+This module contain 1 function that append string in text
+'''
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    List = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    List = []
+for i in argv[1:]:
+    List.append(i)
+save_to_json_file(List, "add_item.json")
