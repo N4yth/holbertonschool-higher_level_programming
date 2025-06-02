a83379523510542b4847c824f7b889ba1535cec7
N4yth <nathan.dupuis31@gmail.com>
2025-06-02 19:44:07 +0200

    add : create python class student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+'''
+This module contain 1 function
+'''
+
+
+class Student:
+    '''
+    class that represent students
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        fonction that return the dictionary representation of this class
+        '''
+        finaldict = {}
+        if attrs:
+            if all(not isinstance(type(attrs[0]), type(i)) for i in attrs):
+                for i in attrs:
+                    if i in self.__dict__:
+                        finaldict[i] = self.__dict__[i]
+                return finaldict
+            else:
+                return self.__dict__
+        else:
+            return self.__dict__
