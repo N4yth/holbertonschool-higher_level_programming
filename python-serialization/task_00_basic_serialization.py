66b7fb850aac9a5edb150b61f01cf220cf562694
N4yth <nathan.dupuis31@gmail.com>
2025-06-03 09:34:47 +0200

    add : create the python module to do a basic serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, 'w', encoding="UTF-8") as fichier:
+        fichier.write(json.dumps(data))
+
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'r', encoding="UTF-8") as fichier:
+        result = fichier.read()
+    return result
