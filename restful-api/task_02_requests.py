ac486f4c558ad549a921ca24c1df030533304a05
N4yth <nathan.dupuis31@gmail.com>
2025-06-09 13:00:28 +0200

    fix : modify the list to dump into a csv file

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -16,8 +16,15 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
+    listed_data = []
     req = requests.get(base_url)
-    print("Status Code: {}".format(req.status_code))
     if req.status_code == 200:
+        data = req.json()
+        for element in data:
+            listed_data.append({
+                "id": element["id"],
+                "title": element["title"],
+                "body": element["body"]
+            })
         with open("posts.csv", "w", encoding="utf-8") as file:
-            json.dump(req.json(), file)
+            json.dump(listed_data, file)
