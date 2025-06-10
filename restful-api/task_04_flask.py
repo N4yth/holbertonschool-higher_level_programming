2d5374d5dfab7013e860ba2fc6479cc3c5711e6f
N4yth <nathan.dupuis31@gmail.com>
2025-06-10 09:12:00 +0200

    feat : create the python module to test flask module

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+global users
+users = {}
+
+
+@app.route("/")
+def home():
+    return "Welcome to Flask API!"
+
+
+@app.route("/data")
+def jsoni():
+    res = []
+    for key in users.keys():
+        res.append(key)
+    return jsonify(res)
+
+
+@app.route("/status")
+def status():
+    return "OK"
+
+
+@app.route("/users/<string:username>")
+def usepart(username):
+    dict_ret = {}
+    if username in users:
+        val = users[username]
+        dict_ret["username"] = username
+        for i in val:
+            dict_ret[i] = val[i]
+        return jsonify(data=dict_ret)
+    else:
+        return jsonify(data={"error": "User not found"}, status=400)
+
+
+@app.route("/add_user", methods=['GET'])
+def add_users():
+    returnval = {"message": "User added", "user": {}}
+    if "username" in request.args.keys() and request.methode == 'GET':
+        users[request.args["username"]] = request.args
+        returnval["user"] = request.args
+        return jsonify(data=returnval, status=201)
+    else:
+        return jsonify(data={"error": "User not found"}, status=400)
+
+
+if __name__ == "__main__":
+    app.run()
