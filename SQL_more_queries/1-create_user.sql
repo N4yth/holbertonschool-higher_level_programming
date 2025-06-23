412fc59a3eed6d83e24663fc05367831244fd89a
N4yth <nathan.dupuis31@gmail.com>
2025-06-23 15:16:09 +0200

    fix : create the passworrd

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create and give permissions to the created user
-CREATE USER IF NOT EXISTS'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' identified by 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
