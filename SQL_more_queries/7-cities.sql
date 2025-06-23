9db5e5ff34a7bf8a4f3e38cd668dc8f6a152d0d0
N4yth <nathan.dupuis31@gmail.com>
2025-06-23 16:05:05 +0200

    feat : create a script that creates a database hbtn_0d_usa and a table cities with foreign and primary key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create and give permissions to the created user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    ADD FOREIGN KEY (id) REFERENCES states(id);
+    UNIQUE (id)
+);
