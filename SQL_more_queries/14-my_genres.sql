fffd113ec6d045c808a01bfb3f92e229c4bbc6a2
N4yth <nathan.dupuis31@gmail.com>
2025-06-24 12:32:02 +0200

    feat : create a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- create and give permissions to the created userSELECT tv_genres.id, tv_genres.name
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter";
