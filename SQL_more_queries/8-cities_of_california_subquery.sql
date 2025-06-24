ec74f17ded90d12a79c85a7e1701ec6b1936d475
N4yth <nathan.dupuis31@gmail.com>
2025-06-24 09:11:03 +0200

    fix : sett to uppercase

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- create and give permissions to the created user
-SELECT id, name from cities WHERE state_id = (SELECT id FROM states WHERE name = "California")
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY id ASC;
