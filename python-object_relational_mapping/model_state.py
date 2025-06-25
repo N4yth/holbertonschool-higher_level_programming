5f0e2ede660afe7ac3e634e28167d3af9d4d3298
N4yth <nathan.dupuis31@gmail.com>
2025-06-25 15:01:03 +0200

    feat : create the python file that contain the definition of State table

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module that define a table with sqlalchemy
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    the class that represente the states table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
