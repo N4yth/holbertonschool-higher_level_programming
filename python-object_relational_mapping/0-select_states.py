71c9d666cca801253f2f4d8deb4f95b2cc89ec7b
N4yth <nathan.dupuis31@gmail.com>
2025-06-25 08:42:22 +0200

    feat : Write a script that lists all states in python

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!./venv/bin/python3
+
+import MySQLdb
+import sqlalchemy
+import sys
+
+def mysqlconnect():
+    db_connection= MySQLdb.connect(
+        host="Localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cirsort = db_connection.cursor()
+    cirsort.execute("SELECT * from states ORDER BY states.id ASC;")
+    listed = cirsort.fetchall()
+    for element in listed:
+        print(element)
+    db_connection.close()
+
+if __name__ == '__main__':
+    mysqlconnect()
