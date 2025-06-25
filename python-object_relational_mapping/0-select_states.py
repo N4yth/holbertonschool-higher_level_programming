7e0a3530373e8a5d7186bf713ea0b763751b90aa
N4yth <nathan.dupuis31@gmail.com>
2025-06-25 08:52:58 +0200

    fix : add documentation and correct style

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-
+"""
+module with one function used to connect
+and display some data from a db
+"""
 import MySQLdb
 import sqlalchemy
 import sys
 
+
 def mysqlconnect():
-    db_connection= MySQLdb.connect(
+    """
+    the function that connect and display the
+    state and their id from the db
+    """
+    db_connection = MySQLdb.connect(
         host="Localhost",
         port=3306,
         user=sys.argv[1],
@@ -18,6 +26,7 @@ def mysqlconnect():
     for element in listed:
         print(element)
     db_connection.close()
+
 
 if __name__ == '__main__':
     mysqlconnect()
