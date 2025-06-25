57bd3b9bf764ee80a0727a9143f8d100fd641bfc
N4yth <nathan.dupuis31@gmail.com>
2025-06-25 08:57:50 +0200

    feat : create a script that lists all states with a name starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+module with one function used to connect
+and display some data from a db
+"""
+import MySQLdb
+import sqlalchemy
+import sys
+
+
+def mysqlconnect():
+    """
+    the function that connect and display the
+    state that have a N in first place int heir name
+    and their id from the db
+    """
+    db_connection = MySQLdb.connect(
+        host="Localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cirsort = db_connection.cursor()
+    cirsort.execute("""SELECT * from states
+        WHERE name LIKE 'N%'
+        ORDER BY states.id ASC;""")
+    listed = cirsort.fetchall()
+    for element in listed:
+        print(element)
+    db_connection.close()
+
+
+if __name__ == '__main__':
+    mysqlconnect()
