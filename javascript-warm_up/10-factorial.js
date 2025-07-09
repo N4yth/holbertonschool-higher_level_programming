499598cb6701adb3e325a2100f44e383ce1b7113
N4yth <nathan.dupuis31@gmail.com>
2025-07-09 17:49:14 +0200

    feat : create a factorial function with recursive

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function factorial (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+console.log(factorial(argv[2]));
