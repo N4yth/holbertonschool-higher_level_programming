88364ceeafde09108e5291f119bf83aa22be824a
N4yth <nathan.dupuis31@gmail.com>
2025-07-09 11:27:36 +0200

    feat : create a js script that print 3 line

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firstvar = 'C is fun';
+const seconvar = 'Python is cool';
+const thirdvar = 'JavaScript is amazing';
+
+console.log(firstvar);
+console.log(seconvar);
+console.log(thirdvar);
