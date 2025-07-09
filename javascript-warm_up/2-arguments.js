29bf924fd15a6f618e651ae4d1bf27469b35b97e
N4yth <nathan.dupuis31@gmail.com>
2025-07-09 16:35:26 +0200

    feat : create a script that print a string base on how many argument is given

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const firstv = 'No argument';
+const seconv = 'Argument found';
+const thirdv = 'Arguments found';
+
+let count = 0;
+
+argv.forEach(function (val, index, argv) {
+    count += 1
+});
+
+if (count == 2)
+{  
+    console.log(firstv);
+}
+else if (count == 3)
+{
+    console.log(seconv);
+}
+else
+{
+    console.log(thirdv);
+}
