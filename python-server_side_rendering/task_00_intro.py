54e873fc27de750ff726ec4812870a8ea74a0906
N4yth <nathan.dupuis31@gmail.com>
2025-07-14 20:11:23 +0200

    feat : create python function that generate invitation

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,22 @@
+
+def generate_invitations(template, attendees):
+    if not isinstance(template, str):
+        return "template must be a string"
+    if not isinstance(attendees, list):
+        return "attendees must be a list"
+    if template == "":
+        return "Template is empty, no output files generated."
+    if attendees == []:
+        return "No data provided, no output files generated."
+
+    for i, element in enumerate(attendees):
+        result = template
+        for index, value in element.items():
+            if (element.get("{}".format(index))):
+                result = result.replace("{"+"{}".format(index)+"}", value)
+            else:
+                result = result.replace("{"+"{}".format(index)+"}",
+                                        "{}:N/A".format(index))
+        with open("output_{}.txt".format(i + 1),
+                  'w', encoding='UTF-8') as fichier:
+            fichier.write(result)
